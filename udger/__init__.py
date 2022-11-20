22eb9afe5092ee4e6f16db9ea01cfe498ed28a73
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-11-20 17:40:41 +0100

    Export UdgerDownloader

--- udger/__init__.py
+++ udger/__init__.py
@@ -1,5 +1,6 @@
 from .parser import UaRequest, Udger
+from .downloader import UdgerDownloader
 
 __version__ = '1.0.0'
 
-__all__ = ['UaRequest', 'Udger']
+__all__ = ['UaRequest', 'Udger', 'UdgerDownloader']
