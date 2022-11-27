7e8ee97ad195c7cf20716ca775e94e18a54bab4e
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-11-27 13:03:14 +0100

    v5.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='udger-v4',
-    version='5.0.1',
+    version='5.0.2',
     license='BSD',
     author='The Udger Team',
     author_email='info@udger.com',
--- udger/__init__.py
+++ udger/__init__.py
@@ -1,6 +1,4 @@
 from .parser import UaRequest, Udger
 from .downloader import UdgerDownloader
 
-__version__ = '5.0.0'
-
 __all__ = ['UaRequest', 'Udger', 'UdgerDownloader']
