fe93d34b6d9b188d1b0e3a3970d51d1150bd1b2e
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-11-27 12:33:24 +0100

    Restructure project

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from os.path import dirname, join
 from setuptools import setup, find_packages
 
-from udger import __version__
-
-
 setup(
     name='udger-v4',
-    version=__version__,
+    version='5.0.0',
     license='BSD',
     author='The Udger Team',
     author_email='info@udger.com',
     description="Fast and reliable User Agent parser and IP classifier for Python",
     long_description_content_type='text/markdown',
     url='https://github.com/udger/udger-python-v4',
-    packages=find_packages(
-        exclude=('udger.tests',),
-    ),
+    packages=['udger'],
     platforms='any',
     zip_safe=True,
     classifiers=[
